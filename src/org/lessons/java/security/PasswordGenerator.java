2343d16d0a6ab41e5e8ffa94242eef663e23cce1
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-29 13:40:16 +0100

    add user info

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -7,6 +7,42 @@ public class PasswordGenerator {
         // creo una variabile per lo scanner
         Scanner scan = new Scanner(System.in);
 
+        // chiedo all'utente il nome
+        System.out.println("Inserisci il tuo nome:");
+        String userName = scan.nextLine();
 
+        // chiedo all'utente il cognome
+        System.out.println("Inserisci il tuo cognome:");
+        String userLastName = scan.nextLine();
+
+        // chiedo all'utente il suo colore preferito
+        System.out.println("Inserisci il tuo colore preferito:");
+        String userColor = scan.nextLine();
+
+        // chiedo all'utente la sua data di nascita
+
+        // giorno
+        System.out.println("In che giorno sei nato?");
+        int userDayOfBirth = scan.nextInt();
+
+        // mese
+        System.out.println("In che mese sei nato? (numero)");
+        int userMonthOfBirth = scan.nextInt();
+
+        // anno
+        System.out.println("In che anno sei nato?");
+        int userYearOfBirth = scan.nextInt();
+
+        // data completa
+        String userDateOfBirth = userDayOfBirth + "/" + userMonthOfBirth + "/" + userYearOfBirth;
+
+        // stampo in console le info dell'utente
+        System.out.println("Nome: " + userName);
+        System.out.println("Cognome: " + userLastName);
+        System.out.println("Colore preferito: " + userColor);
+        System.out.println("Data di nascita: " + userDateOfBirth);
+
+        // chiudo lo scanner
+        scan.close();
     }
 }
