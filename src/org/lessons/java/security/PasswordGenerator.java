20ce96a60f16fe05f40a3959296ee2648f830751
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-29 13:25:12 +0100

    add scanner

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,12 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
+        // creo una variabile per lo scanner
+        Scanner scan = new Scanner(System.in);
+
 
     }
 }
